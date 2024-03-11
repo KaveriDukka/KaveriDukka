be25fc2335819bb4266b874f3b5fdc2ac5f03b72
KaveriDukka <147382631+KaveriDukka@users.noreply.github.com>
2024-03-11 15:01:43 +0530

    Add files via upload

---
+++ MyProject1/src/main/java/DBConfiguration.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConfiguration {
+
+	public Connection DBConfig() {
+		Connection con = null;
+		
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("Driver Connected Successfully.");
+				
+				String url = "jdbc:mysql://localhost:3306/MyProject1";
+				String uname = "root";
+				String pass = "Kaveri123";
+				
+				con = DriverManager.getConnection(url, uname, pass);
+				System.out.println("Db connected successfully.");
+				
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					System.out.println("Driver not Connected."+ e.getMessage());
+				} catch (SQLException e) {
+					e.printStackTrace();
+					System.out.println("Db not connected.");
+				}
+			return con;
+	}
+}
+	
